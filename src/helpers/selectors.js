6bf2091828611075a03c1b30469c401839bb37dd
wentingzoe <ywtzoe@gmail.com>
2022-01-21 19:09:32 -0500

    Update selector.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,5 @@
 export function getAppointmentsForDay (state, day) {
   let output =[];
-  if(state.days.length ===0 ){
-    return output;
-  }
   const filteredDay = state.days.filter(data => data.name === day);
   if(Object.keys(filteredDay).length) {
     const mappedApp = filteredDay[0].appointments.map (data => state.appointments[data])
