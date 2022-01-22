7b32a8992c17a2d05d00564bec253927e17610c3
wentingzoe <ywtzoe@gmail.com>
2022-01-21 21:30:19 -0500

    create getInterview function to fetch interview data and update getappforday function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,10 +2,19 @@ export function getAppointmentsForDay (state, day) {
   let output =[];
   const filteredDay = state.days.filter(data => data.name === day);
   if(Object.keys(filteredDay).length) {
-    const mappedApp = filteredDay[0].appointments.map (data => state.appointments[data])
-    output = mappedApp;
+    output = filteredDay[0].appointments.map (data => state.appointments[data])
   }
-
   return output;
+}
+
+
+export function getInterview(state, interview) {
+  if(!interview){
+    return null;
+  }
+  return {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  }
 }
 
