dfc670ca8a58020f1a14da12623bd46e118badae
wentingzoe <ywtzoe@gmail.com>
2022-01-21 23:45:00 -0500

    create hook folder to add hooks and create useVisualMode(with transition & back functin &  test.js); Install react-testing-library

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+export default function useVisualMode(initial) {
+  // The mode state is what is controlled in useVisualMode
+  const [mode, setMode] = useState(initial);
+  // keep track of the history of the modes
+  const [history, setHistory] = useState([initial]);
+
+  // Replace -- place newMode in place of last mode instead of pushing it after.
+  function transition(newmode, replace = false) {
+    if (replace){
+      history.pop();
+    }
+    history.push(newmode);
+    //setHistory(newmode);
+    setMode(history[history.length -1]);
+  }
+ // Back -- Moves to the previous mode
+  const back =()=>{
+    if (history.length>1){ 
+      // If there is only one item (i.e. first mode), don't pop.
+      history.pop();
+    }
+    setMode(history[history.length -1]);
+  }
+  return { mode,transition,back};
+}
