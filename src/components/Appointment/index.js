199d20b1b02e3a7782901775fcea353b0f6a07de
wentingzoe <ywtzoe@gmail.com>
2022-01-24 23:17:45 -0500

    Add Edit functionality to appointments index and form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -20,6 +20,7 @@ export default function Appointment(props) {
   const SAVING = "SAVING"
   const DELETING = "DELETING"
   const CONFIRM = "CONFIRM"
+  const EDIT = "EDIT"
 
   const {mode, transition, back} = useVisualMode(
     interview ? SHOW : EMPTY
@@ -48,11 +49,12 @@ return (
   <article className="appointment">
     <Header time={time}/>
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-    {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} onDelete={() => transition(CONFIRM)} />}
+    {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} onDelete={() => transition(CONFIRM)} onEdit={() => transition(EDIT)}/>}
     {mode === CREATE && <Form interviewers={interviewers} onSave={save} onCancel={back} />}
     {mode === SAVING && <Status message={"Saving"}/>}
     {mode === DELETING && <Status message={"Deleting"}/>}
     {mode === CONFIRM && <Confirm message={"Are you sure you would like to delete?"} onConfirm={deleteApp} onCancel={back}/>}
+    {mode === EDIT && <Form student={interview.student} interviewers={interviewers} interviewer={interview.interviewer.id} onSave={save} onCancel={back} />}
   </article>
 );
 
