ed0105d36b23bc1e412947fb72d92cd9e0b7c02a
wentingzoe <ywtzoe@gmail.com>
2022-01-24 22:04:58 -0500

    Add confirmation before delte functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,7 +7,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
-// import Confirm from "./Confirm";
+import Confirm from "./Confirm";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
@@ -19,7 +19,7 @@ export default function Appointment(props) {
   const CREATE = "CREATE"
   const SAVING = "SAVING"
   const DELETING = "DELETING"
-  // const CONFIRM = "CONFIRM"
+  const CONFIRM = "CONFIRM"
 
   const {mode, transition, back} = useVisualMode(
     interview ? SHOW : EMPTY
@@ -41,22 +41,18 @@ export default function Appointment(props) {
     cancelInterview(id).then(() => {
       transition(EMPTY)
     })
-    // .catch(()=>{
-    //   transition(EMPTY)
-    // })
   }
-
 
 return (
  
   <article className="appointment">
     <Header time={time}/>
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-    {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} onDelete={deleteApp} />}
+    {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} onDelete={() => transition(CONFIRM)} />}
     {mode === CREATE && <Form interviewers={interviewers} onSave={save} onCancel={back} />}
     {mode === SAVING && <Status message={"Saving"}/>}
     {mode === DELETING && <Status message={"Deleting"}/>}
-    {/* {mode === CONFIRM && <Confirm />} */}
+    {mode === CONFIRM && <Confirm message={"Are you sure you would like to delete?"} onConfirm={deleteApp} onCancel={back}/>}
   </article>
 );
 
