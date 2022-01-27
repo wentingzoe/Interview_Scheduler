891101df3b76b05f061bc4ece0628e40dc724ea4
wentingzoe <ywtzoe@gmail.com>
2022-01-27 18:15:08 -0500

    test breaking it

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -13,7 +13,7 @@ it("defaults to Monday and changes the schedule when a new day is selected", () 
 
   return waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    expect(getByText("Leopold")).toBeInTheDocument();
   });
 });
 
