da56e5992382134206922ce2bb10a98b29f1ea1a
wentingzoe <ywtzoe@gmail.com>
2022-01-27 18:11:27 -0500

    update the form.test; install circleci config

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -41,21 +41,6 @@ describe("Form", () => {
     /* --- onSave is not called */
     expect(onSave).not.toHaveBeenCalled();
   });
-  
-  // it("submits the name entered by the user", () => {
-  //   const onSave = jest.fn();
-  //   const { getByText, getByPlaceholderText } = render(
-  //     <Form interviewers={interviewers} onSave={onSave} />
-  //   );
-  
-  //   const input = getByPlaceholderText("Enter Student Name");
-  
-  //   fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
-  //   fireEvent.click(getByText("Save"));
-  
-  //   expect(onSave).toHaveBeenCalledTimes(1);
-  //   expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-  // });
 
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
